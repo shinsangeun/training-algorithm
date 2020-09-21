66548bff80bcce81949bdb66a37a86c30741cf9a
shinsangeun <s0813se@naver.com>
2020-09-22 04:58:11 +0900

    Add level1 skill check

---
+++ programmers/skillCheck/level1/level1-1.js
@@ -0,0 +1,13 @@
+function solution(a, b) {
+    var answer = 0;
+    if(a <= b){
+        for(let i = a; i<=b; i++){
+            answer = answer + i;
+        }
+    }else if(a > b){
+        for(let i = b; i<=a; i++){
+            answer = answer + i;
+        }
+    }
+    return answer;
+}
---
+++ programmers/skillCheck/level1/level1-2.js
@@ -0,0 +1,10 @@
+function solution(s) {
+    var answer = 0;
+    parseInt(s);
+    if (s>0){
+        answer = s;
+    }else{
+        answer = s;
+    }
+    return parseInt(answer);
+}
